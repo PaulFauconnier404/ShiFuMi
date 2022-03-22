c1ec491a67f65746faef989ad9c17b559be6ff7f
Anth <60937839+anthDev@users.noreply.github.com>
2022-03-22 09:59:09 +0100

    First commit
    Stable dev release of core_release3, which contains the hand gesture detection core and a basic test web page. The next step is to integrate the .js files in the public front-end of the project.

---
+++ core_release3/src/gestures/index.js
@@ -0,0 +1,11 @@
+import ScissorsGesture from './Scissors';
+import PaperGesture from './Paper';
+import RockGesture from './Rock';
+
+export {
+  ScissorsGesture,
+  PaperGesture,
+  RockGesture,
+}
+
+
