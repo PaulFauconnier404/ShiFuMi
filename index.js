07ff7c623bfbafbe3dd3047efd320903505085f4
Charles Musial <64135991+Charles26code@users.noreply.github.com>
2022-05-31 10:33:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,28 +17,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //Récuperation de la forme de l'ID
-function getId(idRequested){
-    const isIntId = parseInt(idRequested);   
-    var good_id = isIntId
-   
-    if(isIntId != idRequested){
-      const ObjectId = require('mongodb').ObjectId; 
-      const id = idRequested;       
-  
-      var good_id = new ObjectId(id);
-    } 
-  
-    return good_id;
+function getId(idRequested) {
+  const isIntId = parseInt(idRequested);
+  var good_id = isIntId
+
+  if (isIntId != idRequested) {
+    const ObjectId = require('mongodb').ObjectId;
+    const id = idRequested;
+
+    var good_id = new ObjectId(id);
   }
-  
+
+  return good_id;
+}
+
 //DATABASE CONNECTION
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://Cnam:Cnam2022@darterking.my4fuyt.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'Shifumi';
 let db
- 
 
-MongoClient.connect(url, function(err, client) {
+
+MongoClient.connect(url, function (err, client) {
   db = client.db(dbName);
   console.log("Connected successfully to server");
 });
@@ -46,88 +46,88 @@ MongoClient.connect(url, function(err, client) {
 
 app.use(express.json())
 
-app.get('/api/user_data', (req,res) => {
-    db.collection('user').find({}).toArray(function(err, docs) {
-        if (err) {
-            console.log(err)
-            throw err
-        }
-        res.status(200).json(docs)
-      }) 
-  })
-  // app.get('/api/user_data/:id', async (req,res) => {
-  //   const id = getId(req.params.id)
-  
-  //   try {
-  //       const docs = await db.collection('user').findOne( {_id : id} )
-  //       res.status(200).json(docs)
-  //   } catch (err) {
-  //       console.log(err)
-  //       throw err
-  //   }
-  // })
-  
-  app.get('/api/user_data/:pseudo', async (req,res) => {
-    const pseudo = req.params.pseudo
-    console.log(pseudo)
-
-    try {
-  
-        const docs = await db.collection('user').findOne( { 'pseudo' : pseudo.toString()} )
-        res.status(200).json(docs)
-    } catch (err) {
-        console.log(err)
-        throw err
+app.get('/api/user_data', (req, res) => {
+  db.collection('user').find({}).toArray(function (err, docs) {
+    if (err) {
+      console.log(err)
+      throw err
     }
+    res.status(200).json(docs)
   })
-  app.post('/api/user_data', async (req,res) => {
+})
+// app.get('/api/user_data/:id', async (req,res) => {
+//   const id = getId(req.params.id)
 
-    try {
-        const userData = req.body
-        const user = await db.collection('user').insertOne(userData)
-        res.status(200).json(user)
+//   try {
+//       const docs = await db.collection('user').findOne( {_id : id} )
+//       res.status(200).json(docs)
+//   } catch (err) {
+//       console.log(err)
+//       throw err
+//   }
+// })
 
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
-    
-  })
-  app.put('/api/user_data/:id', async (req,res) => {
-    try {
-        const id = getId(req.params.id)
-  
-        const userData = req.body
-        const user = await db.collection('user').replaceOne( {_id : id} ,userData)
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
-  })
-  app.patch('/api/user_data/:id', async (req,res) => {
-    try {
-        const id = getId(req.params.id)
-  
-        const userData = req.body
-        const user = await db.collection('user').updateOne( {_id : id} , {$set: userData}, {upsert:true})
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        throw err
-    } 
-  })
-  app.delete('/api/user_data/:id', async (req,res) => {
-    try {
-        const id = getId(req.params.id)
-  
-        const user = await db.collection('user').deleteOne( {_id : id} )
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        throw err
-    } 
-  })
-  
+app.get('/api/user_data/:pseudo', async (req, res) => {
+  const pseudo = req.params.pseudo
+  console.log(pseudo)
+
+  try {
+
+    const docs = await db.collection('user').findOne({ 'pseudo': pseudo.toString() })
+    res.status(200).json(docs)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+})
+app.post('/api/user_data', async (req, res) => {
+
+  try {
+    const userData = req.body
+    const user = await db.collection('user').insertOne(userData)
+    res.status(200).json(user)
+
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+
+})
+app.put('/api/user_data/:id', async (req, res) => {
+  try {
+    const id = getId(req.params.id)
+
+    const userData = req.body
+    const user = await db.collection('user').replaceOne({ _id: id }, userData)
+    res.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+})
+app.patch('/api/user_data/:id', async (req, res) => {
+  try {
+    const id = getId(req.params.id)
+
+    const userData = req.body
+    const user = await db.collection('user').updateOne({ _id: id }, { $set: userData }, { upsert: true })
+    res.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+})
+app.delete('/api/user_data/:id', async (req, res) => {
+  try {
+    const id = getId(req.params.id)
+
+    const user = await db.collection('user').deleteOne({ _id: id })
+    res.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
